0bef5e8099e326efc61460941ff2ceb1d8dfa1b0
Simon <sstur@me.com>
2024-10-20 10:05:11 -0700

    Finally get it working

--- apps/frontend/src/components/ConversationView.tsx
+++ apps/frontend/src/components/ConversationView.tsx
@@ -4,20 +4,18 @@ import { useDaily } from '@daily-co/daily-react';
 import { DailyCall } from '@daily-co/daily-js';
 import { DailyProvider } from '@/providers/DailyProvider';
 import { useCallState } from '@/support/useCallState';
+import { getParticipantList } from '@/support/getParticipantList';
 
 function getRemoteStream(
   callObject: DailyCall | null,
 ): [MediaStreamTrack | undefined, MediaStreamTrack | undefined] {
-  const participants = callObject?.participants();
-  if (participants) {
-    for (const [id, participant] of Object.entries(participants)) {
-      if (id !== 'local') {
-        const audioTrack = participant.tracks.audio.persistentTrack;
-        const videoTrack = participant.tracks.video.persistentTrack;
-        if (audioTrack && videoTrack) {
-          return [audioTrack, videoTrack] as const;
-        }
-      }
+  const participants = getParticipantList(callObject);
+  for (const participant of participants) {
+    if (participant.local) continue;
+    const audioTrack = participant.tracks.audio.persistentTrack;
+    const videoTrack = participant.tracks.video.persistentTrack;
+    if (audioTrack && videoTrack) {
+      return [audioTrack, videoTrack] as const;
     }
   }
   return [undefined, undefined];
@@ -29,11 +27,10 @@ export function ConversationViewContent() {
   const callObject = useDaily();
 
   // This ensures we re-render when the call state changes so that the useEffect
-  // below will be run if necessary.
+  // below will be run when the audioTrack or videoTrack change.
   useCallState();
 
   const [audioTrack, videoTrack] = getRemoteStream(callObject);
-  console.log({ audioTrack, videoTrack });
 
   useEffect(() => {
     const videoEl = videoElRef.current;
@@ -44,7 +41,12 @@ export function ConversationViewContent() {
 
   return (
     <Stack align="center">
-      <video autoPlay playsInline ref={videoElRef} />
+      <video
+        style={{ maxWidth: '400px' }}
+        autoPlay
+        playsInline
+        ref={videoElRef}
+      />
     </Stack>
   );
 }
--- apps/frontend/src/support/useCallState.ts
+++ apps/frontend/src/support/useCallState.ts
@@ -1,8 +1,9 @@
 import { DailyMeetingState as CallState, DailyEvent } from '@daily-co/daily-js';
 import { useDaily } from '@daily-co/daily-react';
 import { useEffect, useState } from 'react';
+import { getParticipantList } from './getParticipantList';
 
-const events = [
+const callStateEvents = [
   'loading',
   'loaded',
   'joining-meeting',
@@ -11,11 +12,19 @@ const events = [
   'error',
 ] as const satisfies Array<DailyEvent>;
 
+const participantStateEvents = [
+  'participant-joined',
+  'participant-updated',
+] as const satisfies Array<DailyEvent>;
+
 // Adapted from https://github.com/daily-demos/call-object-react/blob/5a5092e/src/components/App/App.js
 export function useCallState() {
   const callObject = useDaily();
   const [callState, setCallState] = useState<CallState>(() => {
     return callObject?.meetingState() ?? 'new';
+  });
+  const [participants, setParticipants] = useState(() => {
+    return getParticipantList(callObject);
   });
 
   useEffect(() => {
@@ -26,16 +35,27 @@ export function useCallState() {
       setCallState(callState);
     };
 
-    for (const event of events) {
+    const onParticipantChange = () => {
+      setParticipants(getParticipantList(callObject));
+    };
+
+    for (const event of callStateEvents) {
       callObject.on(event, onMeetingStateChange);
     }
 
+    for (const event of participantStateEvents) {
+      callObject.on(event, onParticipantChange);
+    }
+
     return () => {
-      for (const event of events) {
+      for (const event of callStateEvents) {
         callObject.off(event, onMeetingStateChange);
+      }
+      for (const event of participantStateEvents) {
+        callObject.off(event, onParticipantChange);
       }
     };
   }, [callObject]);
 
-  return callState;
+  return [callState, participants] as const;
 }
