3d0bb3c8c955565e67b3f2f4a012cca6d16be325
aboutphilippe <philippemarketing@gmail.com>
2024-10-20 15:42:23 -0700

    nit

--- apps/backend/src/workflows/assistant.ts
+++ apps/backend/src/workflows/assistant.ts
@@ -13,7 +13,7 @@ export async function assistantWorkflow() {
       llm: {
         model: "proxy",
         api_key: "test",
-        base_url: "https://670d-136-25-28-117.ngrok-free.app",
+        base_url: "https://4163-32-132-92-198.ngrok-free.app",
       },
       vqa: {
         enable_vision: true,
