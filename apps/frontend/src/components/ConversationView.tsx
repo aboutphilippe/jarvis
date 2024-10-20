f457eb289d297afa73ff0210d0503e9fbcead416
Simon <sstur@me.com>
2024-10-19 20:49:50 -0700

    Get correct video displaying

--- apps/frontend/src/components/ConversationView.tsx
+++ apps/frontend/src/components/ConversationView.tsx
@@ -1,27 +1,42 @@
 import { Stack } from '@mantine/core';
 import { useEffect, useRef } from 'react';
-import { useLocalSessionId, useVideoTrack } from '@daily-co/daily-react';
+import { useDaily } from '@daily-co/daily-react';
+import { DailyCall } from '@daily-co/daily-js';
 import { DailyProvider } from '@/providers/DailyProvider';
 
+function getFirstNonLocalVideoTrack(callObject: DailyCall | null) {
+  const participants = callObject?.participants();
+  if (!participants) {
+    return;
+  }
+  for (const [id, participant] of Object.entries(participants)) {
+    if (id !== 'local') {
+      const videoTrack = participant.tracks.video.persistentTrack;
+      if (videoTrack) {
+        return videoTrack;
+      }
+    }
+  }
+}
+
 export function ConversationViewContent() {
-  const localVideoElement = useRef<HTMLVideoElement>(null);
+  const videoElRef = useRef<HTMLVideoElement>(null);
 
-  const sessionId = useLocalSessionId();
-  const videoTrack = useVideoTrack(sessionId);
+  const callObject = useDaily();
 
-  const { persistentTrack } = videoTrack;
+  const videoTrack = getFirstNonLocalVideoTrack(callObject);
+  console.log({ videoTrack });
 
   useEffect(() => {
-    const el = localVideoElement.current;
-    if (!persistentTrack || !el) {
-      return;
+    const videoEl = videoElRef.current;
+    if (videoEl && videoTrack) {
+      videoEl.srcObject = new MediaStream([videoTrack]);
     }
-    el.srcObject = persistentTrack && new MediaStream([persistentTrack]);
-  }, [persistentTrack]);
+  }, [videoTrack]);
 
   return (
     <Stack align="center">
-      <video autoPlay muted playsInline ref={localVideoElement} />
+      <video autoPlay muted playsInline ref={videoElRef} />
     </Stack>
   );
 }
